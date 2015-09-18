ca18ae7fbced728beebc74be1bdc0910de4c0774
unknown <labdismov@113DAC26G.itam.mx>
2015-09-18 13:47:36 -0500

    Period selection modified, more general now

---
+++ CulturasPrehispanicas/Assets/Scripts/PeriodController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PeriodController : MonoBehaviour {
+
+    public Text period;
+    public Text info;
+    string[] periodNames = new string[] { "Preclásico", "Clásico", "Posclásico" };
+    string[] periodDescriptions = new string[] { "Primer periodo", "Segundo periodo", "Tercer periodo" };
+    int currentPeriod;
+
+    void Start () {
+        currentPeriod = PlayerPrefs.GetInt("CurrentPeriod",0);
+        setValues();
+	}
+
+    void setValues(){
+        period.text = periodNames[currentPeriod];
+        info.text = periodDescriptions[currentPeriod];
+    }
+
+	public void changePeriod(int increment){
+        currentPeriod += increment;
+        if (currentPeriod >= 0 && currentPeriod < periodNames.Length) setValues();
+        if (currentPeriod < 0) currentPeriod = 0;
+        if (currentPeriod >= periodNames.Length) currentPeriod = periodNames.Length - 1;
+        PlayerPrefs.SetInt("CurrentPeriod",currentPeriod);
+    }
+
+	void Update () {
+	
+	}
+}
